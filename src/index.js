e585df4f64975f189fa4c7d50526ffb64aa665bd
Anirban Jash <98976464+anirbanjash@users.noreply.github.com>
2023-04-21 01:13:38 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+var number = 7;
+ReactDOM.render(
+  <div>
+    <h1>Hello Anirban </h1>
+    <p>You lucky number is {number}</p>
+  </div>,
+  document.getElementById("root")
+);
